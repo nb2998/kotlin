03360808197dbae00f7416713e377a8cd2ab5639
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2017-09-16 07:43:42 +0300

    Setup project versions
    Build parameters (with corresponding project properties):
    - build.number (buildNumber) - build number from build server, goes into manifest, by default snapshot
    - deployVersion (kotlinVersion, project.version) - version of artifacts, by default build.number
    - bootstrap.kotlin.version (bootstrapKotlinVersion) - version of bootstrap compiler

--- buildSrc/src/main/kotlin/artifacts.kt
+++ buildSrc/src/main/kotlin/artifacts.kt
@@ -130,15 +130,16 @@ fun Project.publish(body: Upload.() -> Unit = {}): Upload {
 }
 
 fun Project.ideaPlugin(subdir: String = "lib", body: AbstractCopyTask.() -> Unit): Copy {
+    val thisProject = this
     val pluginTask = task<Copy>("ideaPlugin") {
         body()
         into(File(rootProject.extra["ideaPluginDir"].toString(), subdir).path)
-        rename("-${java.util.regex.Pattern.quote(rootProject.extra["build.number"].toString())}", "")
+        rename("-${java.util.regex.Pattern.quote(thisProject.version.toString())}", "")
     }
 
     task("idea-plugin") {
         dependsOn(pluginTask)
-    }
+}
 
     return pluginTask
 }
@@ -163,7 +164,7 @@ fun Project.dist(targetDir: File? = null,
             from(it)
             if (targetName != null) {
                 rename(it.outputs.files.singleFile.name, targetName)
-            }
+        }
         }
         rename("-${java.util.regex.Pattern.quote(rootProject.extra["build.number"].toString())}", "")
         into(targetDir ?: distLibDir)
@@ -185,7 +186,7 @@ fun Jar.setupPublicJar(classifier: String = "", classifierDescr: String? = null)
         put("Built-By", project.rootProject.extra["manifest.impl.vendor"])
         put("Implementation-Vendor", project.rootProject.extra["manifest.impl.vendor"])
         put("Implementation-Title", "${project.description} ${classifierDescr ?: classifier}".trim())
-        put("Implementation-Version", project.rootProject.extra["build.number"])
+        put("Implementation-Version", project.rootProject.extra["buildNumber"])
     }
 //    from(project.configurations.getByName("build-version").files, action = { into("META-INF/") })
 }
